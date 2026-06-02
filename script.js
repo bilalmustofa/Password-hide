7c69aa606269a0f7985d0e62e7e6797daffbc8be
bilal <bilalmustofa146@gmail.com>
2026-06-02 21:54:10 +0300

    Add password strength message by password length

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const passwordInput = document.getElementById('passwordInput');
 const eyeIcon = document.getElementById('eyeIcon');
 
+// Show and Hide Password
 eyeIcon.addEventListener('click', () => {
 
      // Check the current type 
@@ -20,4 +21,31 @@ eyeIcon.addEventListener('click', () => {
          eyeIcon.classList.add('fa-eye-slash');
     }
    
-})
+});
+
+// Password strength by the length of password
+const message = document.getElementById('message');
+const passwordBox = document.querySelector('.password-box');
+
+passwordInput.addEventListener('input', () => {
+    if(passwordInput.value.length > 0) {
+        message.style.display = 'block';
+    } else {
+        message.style.display = 'none';
+        passwordBox.style.border = '';
+    }
+
+    if(passwordInput.value.length < 4 && passwordInput.value.length > 0) {
+        passwordBox.style.border = '1.5px solid #ff5925';
+        message.textContent = "Password is Weak";
+        message.style.color = '#ff5925';
+    } else if (passwordInput.value.length >= 4 && passwordInput.value.length < 8) {
+        passwordBox.style.border = "1.5px solid yellow";
+        message.textContent = "Password is Medium";
+        message.style.color = 'yellow';
+    } else if(passwordInput.value.length >= 8) {
+        passwordBox.style.border = '1.5px solid #26d730';
+        message.textContent = "Password is Strong";
+        message.style.color = '#26d730';
+    }
+});
